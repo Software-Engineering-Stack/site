c5d9043bd440c7587854c4a752d1952a66669edd
Wesley <wesleyqueirozdeveloper@gmail.com>
2025-02-28 21:50:59 -0300

    Add axios for API calls and implement user data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import Container from '@mui/material/Container';
+import { useEffect, useState } from "react";
+import axios from "axios";
 export default function App() {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/WesleyQDev")
+      .then((response) => {
+        setUser(response.data)
+        setLoading(false)
+      })
+      .catch((error) => console.log(error))
+      .finally(console.log("Request completed"))
+  },[])
+
+  console.log(user, "user");
   return (
     <div>
-      <h1>My App</h1>
-      <p>My first React app!</p>
+      {
+        loading ?  (<h1>Carregando informações...</h1>) :(
+          <>
+            <input name="myInput" />
+            <p>{user.login}</p>
+            <img src={user.avatar_url} />
+          </>
+      )
+      }
     </div>
   );
 }
